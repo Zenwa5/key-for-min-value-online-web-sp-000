96fab89231bb21d0bbe0c7c2ce4d3e1b81f75b90
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-19 16:52:33 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,9 +6,9 @@ def key_for_min_value(name_hash)
   p = nil
   name_hash.each do |key, value|
     if n = nil || p > value
-      n = value
-      p = key
+      p = value
+      n = key
     end
   end
-  return p
+  return n
 end
