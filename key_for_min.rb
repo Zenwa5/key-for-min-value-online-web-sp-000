0d7251f1f57939df60e6837640ea5a3e00be4ad9
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-19 16:47:34 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.detect do |key, value|
+  name_hash.each do |key, value|
     n = 0
     if value < n
       return key
